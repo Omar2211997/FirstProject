54dc28d11f901eccedcac229515275e36103520d
Omar TERRO <githubomarterro1997@gmail.com>
2020-08-24 23:04:48 +0200

    change build

--- JAVAProject/Build.bat
+++ JAVAProject/Build.bat
@@ -1,7 +1,10 @@
+set WORKSPACE=%CD%
 
+cd C:\Program Files\Java\jdk1.8.0_261\bin
 
-javac HelloWorldExample.java
+javac %WORKSPACE%\HelloWorldExample.java
 
+cd %WORKSPACE%
 
 java  HelloWorldExample >>out.txt
 
